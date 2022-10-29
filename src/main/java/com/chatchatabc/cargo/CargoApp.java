c2efb0ce48fec4c10afd7edb452f5029d40d14f6
linux_china <libing.chen@gmail.com>
2022-10-29 11:26:46 +0800

    app: CargoApp with Spring Boot

---
+++ src/main/java/com/chatchatabc/cargo/CargoApp.java
@@ -0,0 +1,11 @@
+package com.chatchatabc.cargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CargoApp.class, args);
+    }
+}
