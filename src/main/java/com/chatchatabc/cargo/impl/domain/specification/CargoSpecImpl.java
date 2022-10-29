8fe2e3840939354e92fbf1e8f7df3aeab1c45ae6
linux_china <libing.chen@gmail.com>
2022-10-29 11:29:05 +0800

    ddd(spec): CargoSpec implementation

---
+++ src/main/java/com/chatchatabc/cargo/impl/domain/specification/CargoSpecImpl.java
@@ -0,0 +1,17 @@
+package com.chatchatabc.cargo.impl.domain.specification;
+
+import com.chatchatabc.cargo.domain.specification.CargoSpec;
+
+import java.util.Optional;
+
+/**
+ * Cargo Specification implementation
+ *
+ * @author linux_china
+ */
+public class CargoSpecImpl implements CargoSpec {
+    @Override
+    public Optional<Long> isEmailUnique(String email) {
+        return Optional.empty();
+    }
+}
