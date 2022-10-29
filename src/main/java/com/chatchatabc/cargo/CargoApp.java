2a0f6ff2c2f32f075f64f67cec6bb626e1bdea6a
linux_china <libing.chen@gmail.com>
2022-10-29 12:04:32 +0800

    chore: add @EnableAsync for app

--- src/main/java/com/chatchatabc/cargo/CargoApp.java
+++ src/main/java/com/chatchatabc/cargo/CargoApp.java
@@ -2,8 +2,10 @@ package com.chatchatabc.cargo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CargoApp {
     public static void main(String[] args) {
         SpringApplication.run(CargoApp.class, args);
