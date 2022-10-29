1ef8ceb68fc57cc2a68627bf636f0c1d52499488
linux_china <libing.chen@gmail.com>
2022-10-29 11:43:51 +0800

    chore: polishing

--- src/test/java/com/chatchatabc/cargo/impl/domain/repository/CargoRepositoryImplTest.java
+++ src/test/java/com/chatchatabc/cargo/impl/domain/repository/CargoRepositoryImplTest.java
@@ -2,7 +2,6 @@ package com.chatchatabc.cargo.impl.domain.repository;
 
 import com.chatchatabc.cargo.SpringBootBaseTest;
 import com.chatchatabc.cargo.domain.model.Cargo;
-import com.chatchatabc.cargo.domain.repository.CargoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CargoRepositoryImplTest extends SpringBootBaseTest {
     @Autowired
-    private CargoRepository cargoRepository;
+    private CargoRepositoryImpl cargoRepository;
 
     @Test
     public void testFindCargoById() {
