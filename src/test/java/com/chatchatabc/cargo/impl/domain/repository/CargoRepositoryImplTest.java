b7057c782b768746561aa05c62724ada4f9a8d42
linux_china <libing.chen@gmail.com>
2022-10-29 11:38:25 +0800

    test: CargoRepositoryImpl test

---
+++ src/test/java/com/chatchatabc/cargo/impl/domain/repository/CargoRepositoryImplTest.java
@@ -0,0 +1,22 @@
+package com.chatchatabc.cargo.impl.domain.repository;
+
+import com.chatchatabc.cargo.SpringBootBaseTest;
+import com.chatchatabc.cargo.domain.model.Cargo;
+import com.chatchatabc.cargo.domain.repository.CargoRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CargoRepositoryImplTest extends SpringBootBaseTest {
+    @Autowired
+    private CargoRepository cargoRepository;
+
+    @Test
+    public void testFindCargoById() {
+        final Cargo cargo = cargoRepository.findCargoById(1L);
+        assertThat(cargo).isNotNull();
+        System.out.println(cargo.getTrackingId());
+    }
+}
