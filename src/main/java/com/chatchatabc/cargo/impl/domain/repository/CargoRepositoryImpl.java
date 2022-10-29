d7585d71849b945f0b1270a5c1c8e6dee4b6610d
linux_china <libing.chen@gmail.com>
2022-10-29 12:09:04 +0800

    chore: polishing

--- src/main/java/com/chatchatabc/cargo/impl/domain/repository/CargoRepositoryImpl.java
+++ src/main/java/com/chatchatabc/cargo/impl/domain/repository/CargoRepositoryImpl.java
@@ -7,11 +7,14 @@ import com.chatchatabc.cargo.domain.repository.CargoRepository;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public class CargoRepositoryImpl implements CargoRepository {
+
     @Nullable
     @Override
     public Cargo findCargoById(Long id) {
-        return new Cargo(1L, "ups-20210309110234", new Location(1L, "LA"));
+        return new Cargo(1L, UUID.randomUUID().toString(), new Location(1L, "Davao"));
     }
 }
