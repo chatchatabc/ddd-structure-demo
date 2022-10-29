53e2c61080d612733c8d0bbc937a7e3cb126f044
linux_china <libing.chen@gmail.com>
2022-10-29 11:24:58 +0800

    ddd(event): CargoEvent

---
+++ src/main/java/com/chatchatabc/cargo/domain/event/CargoEvent.java
@@ -0,0 +1,23 @@
+package com.chatchatabc.cargo.domain.event;
+
+import com.chatchatabc.cargo.domain.model.Cargo;
+import org.jmolecules.event.annotation.DomainEvent;
+
+/**
+ * Cargo event
+ *
+ * @author linux_china
+ */
+@DomainEvent
+public class CargoEvent {
+    public static String placed_type = "placed";
+    public static String SHIPPED_TYPE = "created";
+    public static String ARRIVED_TYPE = "blocked";
+
+    private String type;
+    private Cargo cargo;
+
+    public CargoEvent(String type, Cargo cargo) {
+
+    }
+}
