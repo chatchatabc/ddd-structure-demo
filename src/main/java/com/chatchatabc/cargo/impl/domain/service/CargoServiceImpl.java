0f104fbf9b7b197252abbc60152419ae86f26da6
linux_china <libing.chen@gmail.com>
2022-10-29 12:12:17 +0800

    ddd(event): add Event firing

--- src/main/java/com/chatchatabc/cargo/impl/domain/service/CargoServiceImpl.java
+++ src/main/java/com/chatchatabc/cargo/impl/domain/service/CargoServiceImpl.java
@@ -1,14 +1,23 @@
 package com.chatchatabc.cargo.impl.domain.service;
 
+import com.chatchatabc.cargo.domain.event.CargoEvent;
 import com.chatchatabc.cargo.domain.model.Cargo;
 import com.chatchatabc.cargo.domain.service.CargoService;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CargoServiceImpl implements CargoService {
+    @Autowired
+    ApplicationEventPublisher eventPublisher;
+
     @Override
     public Long createCargo(@NotNull Cargo cargo) {
-        return 1L;
+        Long id = 1L;
+        cargo.setId(id);
+        eventPublisher.publishEvent(new CargoEvent(CargoEvent.PLACED_TYPE, cargo));
+        return id;
     }
 }
