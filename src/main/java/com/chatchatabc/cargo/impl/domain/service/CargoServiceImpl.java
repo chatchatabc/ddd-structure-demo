ae115a3df37caeccf058d491c345b79971bd42d9
linux_china <libing.chen@gmail.com>
2022-11-02 15:25:40 +0800

    ddd(service): add cargoRepository to insert cargo

--- src/main/java/com/chatchatabc/cargo/impl/domain/service/CargoServiceImpl.java
+++ src/main/java/com/chatchatabc/cargo/impl/domain/service/CargoServiceImpl.java
@@ -2,6 +2,7 @@ package com.chatchatabc.cargo.impl.domain.service;
 
 import com.chatchatabc.cargo.domain.event.CargoEvent;
 import com.chatchatabc.cargo.domain.model.Cargo;
+import com.chatchatabc.cargo.domain.repository.CargoRepository;
 import com.chatchatabc.cargo.domain.service.CargoService;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,14 @@ import org.springframework.stereotype.Service;
 public class CargoServiceImpl implements CargoService {
     @Autowired
     ApplicationEventPublisher eventPublisher;
+    @Autowired
+    CargoRepository cargoRepository;
 
     @Override
     public Long createCargo(@NotNull Cargo cargo) {
         Long id = 1L;
         cargo.setId(id);
+        cargoRepository.insertCargo(cargo);
         eventPublisher.publishEvent(new CargoEvent(CargoEvent.PLACED_TYPE, cargo));
         return id;
     }
