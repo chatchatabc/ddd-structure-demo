2ba01382336421706486ece28f2ce973f4955a40
Libing Chen <libing.chen@gmail.com>
2022-12-11 09:39:26 +0800

    chore: add @RequestMapping("/cargo") for class

--- src/main/java/com/chatchatabc/cargo/application/rest/CargoController.java
+++ src/main/java/com/chatchatabc/cargo/application/rest/CargoController.java
@@ -6,6 +6,7 @@ import com.chatchatabc.cargo.domain.repository.CargoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,11 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author linux_china
  */
 @RestController
+@RequestMapping("/cargo")
 public class CargoController {
     @Autowired
     private CargoRepository cargoRepository;
 
-    @GetMapping("/cargo/{id}")
+    @GetMapping("/{id}")
     public Cargo show(@PathVariable Long id) throws CargoNotFoundException {
         final Cargo cargo = cargoRepository.findCargoById(id);
         if (cargo == null) {
